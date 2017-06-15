84cfa8b2cd7026c40628ce5f7a03ffb8c7681c12
Tealc Wu <tealcwu@hotmail.com>
2017-06-15 13:48:20 +0800

    QuizGame2: Add high score with playerprefs.

--- Topics/Scripting/QuizGame2/Assets/Scripts/DataController.cs
+++ Topics/Scripting/QuizGame2/Assets/Scripts/DataController.cs
@@ -5,16 +5,48 @@ using UnityEngine.SceneManagement;
 public class DataController : MonoBehaviour 
 {
 	public RoundData[] allRoundData;
-	
+	private PlayerProgress playerProgress;
+
+
 	void Start ()  
 	{
 		DontDestroyOnLoad (gameObject);
-		
+		LoadPlayerProgress ();
+
 		SceneManager.LoadScene ("MenuScreen");
 	}
 	
 	public RoundData GetCurrentRoundData()
 	{
 		return allRoundData [0];
+	}
+
+	public void SubmitNewPlayerScore(int newScore)
+	{
+		if (newScore > playerProgress.highestScore) 
+		{
+			playerProgress.highestScore = newScore;
+			SavePlayerProgress ();
+		}
+	}
+
+	public int GetHighestPlayerScore()
+	{
+		return playerProgress.highestScore;
+	}
+
+	private void LoadPlayerProgress()
+	{
+		playerProgress = new PlayerProgress ();
+
+		if (PlayerPrefs.HasKey ("highestScore")) {
+		
+			playerProgress.highestScore = PlayerPrefs.GetInt ("highestScore");
+		}
+	}
+
+	private void SavePlayerProgress()
+	{
+		PlayerPrefs.SetInt ("highestScore", playerProgress.highestScore);
 	}
 }
